acab7cf2895da24936cfb2b644a0f8f131a1b1d1
Makonede <61922615+Makonede@users.noreply.github.com>
2023-01-31 12:32:40 -0800

    _main. init, fini are used

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <cstdlib>
 
 
-[[noreturn]] [[maybe_unused]] void _main([[maybe_unused]] void *unused) {
+[[noreturn]] void _main([[maybe_unused]] void *unused) {
   auto trigger = &triggerAddr;
   sead::SafeString eventName("LinkCannon");
 
@@ -18,7 +18,7 @@
 }
 
 
-[[maybe_unused]] extern "C" void init() {
+extern "C" void init() {
   static nn::os::ThreadType mainThread;
   constexpr auto stackSize = 0x80000uz;
   auto stack = aligned_alloc(0x1000uz, stackSize);
@@ -34,4 +34,4 @@
 }
 
 
-[[maybe_unused]] extern "C" void fini() {}
+extern "C" void fini() {}
