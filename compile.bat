1b1c40e44002e7862fba36884306f2866fcbc74a
Makonede <61922615+Makonede@users.noreply.github.com>
2023-02-02 12:22:03 -0800

    Remove cheat: Part 2

--- compile.bat
+++ compile.bat
@@ -16,19 +16,16 @@ rem  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 set VERSION=0.1
 set TID=01007EF00011E000
-set BID=8E9978D50BDD20B4
 set RELEASE_PATH=release\atmosphere\contents\%TID%
 
 make -C build -f ..\Makefile || exit /b
 rd /s /q release || exit /b
 md %RELEASE_PATH%\exefs || exit /b
 md %RELEASE_PATH%\romfs\System || exit /b
-md %RELEASE_PATH%\cheats || exit /b
 echo 1.6.0-LC%VERSION% > %RELEASE_PATH%\romfs\System\Version.txt || exit /b
 copy build\linkcannon.nso %RELEASE_PATH%\exefs\subsdk9 || exit /b
 copy build\app.npdm %RELEASE_PATH%\exefs\main.npdm || exit /b
 robocopy romfs %RELEASE_PATH%\romfs /e /mt
-copy cheat.txt %RELEASE_PATH%\cheats\%BID%.txt || exit /b
 
 @echo off
 echo. || exit /b
