2f5967808a418baa7ec9d323e95569a17b062115
Makonede <61922615+Makonede@users.noreply.github.com>
2023-02-03 11:22:45 -0800

    Add ZR to button combination: Part 4

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <cstdlib>
 
 
-constexpr auto BUTTON_COMBO = 0x00010060;
+constexpr auto BUTTON_COMBO = btn::ZR | btn::UP | btn::R3;
 
 
 // Main loop thread
