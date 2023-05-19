ab777e5055fc51331dc13a8c94d4d0d7b652bae0
Makonede <61922615+Makonede@users.noreply.github.com>
2023-05-19 09:59:31 -0700

    Move config.toml to config/botw-link.toml (part 3)
    Signed-off-by: Makonede <61922615+Makonede@users.noreply.github.com>

--- compile.bat
+++ compile.bat
@@ -29,7 +29,7 @@ py -m pip install -Ur tools\botw-link\requirements.txt || exit /b
 py -m pip install -U vidua || exit /b
 if exist build rd /s /q build || exit /b
 md build || exit /b
-py -3 tools\botw-link config.toml -cvV 160 || exit /b
+py -3 tools\botw-link config\botw-link.toml -cvV 160 || exit /b
 if exist release rd /s /q release || exit /b
 if exist patched rd /s /q patched || exit /b
 md %RELEASE_PATH%\exefs || exit /b
