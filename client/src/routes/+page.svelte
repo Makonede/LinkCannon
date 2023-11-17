823fde1b0a3b02a6a38f686b54b2a702847ec1f8
Mako <61922615+Makonede@users.noreply.github.com>
2023-11-17 10:23:15 -0800

    Stricter port validation
    Signed-off-by: Mako <61922615+Makonede@users.noreply.github.com>

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
   const PORT = 52617
   let address: string
-  let port: string
+  let portStr: string
   let err = ''
   let color = 'slate'
 </script>
@@ -12,15 +12,23 @@
 <input type='text' name='address' required maxlength=39 bind:value={address}>
 <label for='port'>Port</label>
 <input type='text' inputmode='numeric' name='port' maxlength=5
-  pattern='[^\D0]\d{'{'}0,4{'}'}' placeholder={`${PORT}`}
-  bind:value={port}>
+  pattern='[^\D0]\d{'{'}0,3{'}'}|[1-6]\d{'{'}4{'}'}' placeholder={`${PORT}`}
+  bind:value={portStr}>
 
 <button on:click={async () => {
+  // Validate port
+  const port = portStr ? parseInt(portStr) : PORT
+  if (!port) {
+    err = 'Invalid port'
+    color = 'red'
+    return
+  }
+
   err = 'Connecting...'
   color = 'slate'
 
   try {
-    await invoke('connect', { address, port: parseInt(port) || PORT })
+    await invoke('connect', { address, port })
     err = 'Connected!'
     color = 'green'
   }
